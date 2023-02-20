6e1ec8bc106b14067ac7d90cde3167e1396a67b5
Efendi <tanefendi.97@gmail.com>
2023-02-20 15:29:59 +0700

    finished fibonacci function using iteration and recursion

--- main.js
+++ main.js
@@ -1,53 +1,29 @@
-// function fibs(num){
-//   const result=[0,1];
-//   for(let i=0;i<num-2;i++){
-//     result.push(result[i]+result[i+1]);
-//   }
-//   console.log(result);
-// }
-// fibs(8);
-
-// const result=[0,1];
-// let i=0;
-// function fibsRec(num){
-//   if(num===2){
-//     return
-//   }
-//   result.push((result[i]+result[i+1]))
-//   i++;
-//   fibsRec(num-1);
-// }
-
-// fibsRec(25);
-// console.log(result)
-
-// function mergeSort(arr){
-//   if(arr.length===1){
-//     return
-//   }
-// }
-
-// mergeSort([9,8,7,6,5,4,3,2,1]);
-
-// const result=[];
-// function recursion(num){
-//   if(num===1){
-//     result[num]=1;
-//     return 1;
-//   }else if(num===0){
-//     result[num]=0;
-//     return 0;
-//   }
-//   return result[num]=(recursion(num-1)+recursion(num-2));
-// }
-
-// console.log(recursion(8));
-// console.log(result);
-
-function mergeSort(arr){
-  if(arr.length===1){
-    return
+/*=========================*/
+/*FIBONACCI USING ITERATION*/
+/*=========================*/
+function fibs(num){
+  const result=[0,1];
+  for(let i=0;i<num-2;i++){
+    result.push(result[i]+result[i+1]);
   }
+  console.log(result);
+}
+fibs(8); // will log [0,1,1,2,3,5,8,13]
+/*=========================*/
+/*FIBONACCI USING RECURSION*/
+/*=========================*/
+function fibonacciRecursion(num){
+  const result=[];
+    function recursion(num){
+      if(num<=2){
+        result[num-1]=num-1;
+        return num-1;
+      }
+      return result[num-1]=(recursion(num-1)+recursion(num-2));
+    }
+  recursion(num);
+  return result;
 }
 
-mergeSort([9,1,2,7,3,4,5,8,6])
+console.log(fibonacciRecursion(8)); // will log [0,1,1,2,3,5,8,13]
+/*=========================*/
